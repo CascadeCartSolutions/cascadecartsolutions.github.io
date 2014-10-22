b962b78731ee11ab603fbafe6425d9b2ab3c7cbf
joebeone <jbenentt@webrapids.com>
2014-10-22 09:58:50 -0400

    changed parameters to quick lookup

--- js/quick_lookup.js
+++ js/quick_lookup.js
@@ -1,9 +1,26 @@
-function getParameterByName(name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
+var QueryString = function () {
+  // This function is anonymous, is executed immediately and
+  // the return value is assigned to QueryString!
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    	// If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = pair[1];
+    	// If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]], pair[1] ];
+      query_string[pair[0]] = arr;
+    	// If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(pair[1]);
+    }
+  }
+    return query_string;
+} ();
+
 
 String.prototype.replaceAll = function(search, replace)
 {
@@ -17,16 +34,20 @@ String.prototype.replaceAll = function(search, replace)
 
 
 startLookup = function(){
-    var ticket_id = getParameterByName('ticket');
+    var ticket_id = QueryString.ticket_id;
+    var site = QueryString.site;
+
 
     $.ajax({
                 type: 'GET',
-                url: "http://field1.gocartlogic.com/api/2/ticket/detail/"  + ticket_id,
+                url: "http://" + site + ".gocartlogic.com/api/2/ticket/detail/"  + ticket_id,
                 dataType: 'json',
                 success: function (data) {
                 console.log(data.status);
                     date_last_attempted = new Date(data.date_last_attempted);
                     date_completed = new Date(data.date_completed);
+                    console.log(data);
+                    console.log(data.location);
 
                     if (data.status == "OPEN"){
                         $("#service_info").html("STATUS: <b>" + data.status + "</b><br> ADDRESS: <b>" + data.location.replaceAll("-", " "));
